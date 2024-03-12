2736653a4267d90388b93120d13087be48ff0194
cgnanken <cheick-ahmed.gnankene@capgemini.com>
2024-03-12 23:06:38 +0100

    init

---
+++ src/test/java/com/capgemini/pocelasticsearch/PocElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.pocelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
